0d454aff302ae3a0c171fb50f995c94d01753245
Aravind427441 <ceebeearavind@gmail.com>
2024-09-01 12:49:39 +0530

    Initial commit

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket
+
+HEADER=64
+PORT=5050
+FORMAT='utf-8'
+DISCONNECT_MESSAGE="!DISCONNECT"
+SERVER=socket.gethostbyname(socket.gethostname())
+ADDR=(SERVER,PORT)
+client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)  # Encode the message length to bytes
+    send_length += b' ' * (HEADER - len(send_length))  # Pad the message length to fit the HEADER size
+    client.send(send_length)
+    client.send(message)
+
+
+send("Hello World!")
+
+
---
+++ main.py
@@ -0,0 +1,49 @@
+import socket
+import threading
+import time
+
+# time.sleep(3)
+# print("hello")
+HEADER=64
+PORT=5050
+# SERVER="192.168.11.1"(below line of code does the same)
+SERVER=socket.gethostbyname(socket.gethostname())
+
+ADDR=(SERVER,PORT)
+FORMAT='utf-8'
+DISCONNECT_MESSAGE="!DISCONNECT"
+
+
+
+
+
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn,addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    connected=True
+    while connected:
+        msg_length=conn.recv(HEADER).decode(FORMAT)   #decodes message from byte format to string
+        if msg_length:
+            msg_length=int(msg_length)
+            msg=conn.recv(msg_length).decode(FORMAT)
+            if msg==DISCONNECT_MESSAGE:
+                connected=False
+            print(f"[{addr}] {msg}")
+
+    conn.close()
+
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+       conn,addr = server.accept()
+       thread=threading.Thread(target=handle_client,args=(conn,addr))
+       thread.start()
+       print(f"[ACTIVE CONNECTIONS] {threading.active_count()-1}")
+
+print("[STARTING]  server is starting")
+start()
